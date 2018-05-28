22645e09c03b8a1d7c1c505346646dbab1a2a285
Qianyi Shu <qs4617@matrix01.doc.ic.ac.uk>
2018-05-28 11:01:54 +0100

    passing most of data processing instruction

--- src/assemble/assemble.h
+++ src/assemble/assemble.h
@@ -24,8 +24,8 @@ void assembleMultiplyInstruction(FILE* fpOutput, struct Token*);
 void assembleSingleDataInstruction(FILE* fpOutput, struct Token*);
 void assembleBranchInstruction(FILE* fpOutput, struct Token*);
 
-uint8_t getOperand2Immediate(uint32_t operand2Val);
-
+uint16_t getOperand2Immediate(uint32_t operand2Val);
+uint16_t getOperand2ShiftRegister(uint32_t operand2Val);
 struct SymbolTable* initializeInstructionCodeTable();
 
 
