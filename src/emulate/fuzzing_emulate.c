6fa1e09c029f54c01a55b7fe0f082e43e3a36709
Francis Nixon <13nixonf@gmail.com>
2018-05-30 00:47:08 +0100

    added fuzzing for target emulate

---
+++ src/emulate/fuzzing_emulate.c
@@ -0,0 +1,16 @@
+//
+// Created by fpn17 on 27/05/18.
+//
+
+
+#include <stdint.h>
+#include <glob.h>
+#include <stdlib.h>
+#include "emulate_main.h"
+
+int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+//   DoSomethingInterestingWithMyAPI(Data, Size);
+  struct EmulatorState *state = malloc(sizeof(struct EmulatorState));
+  emulate(state,Data,Size);
+  return 0;  // Non-zero return values are reserved for future use.
+}
