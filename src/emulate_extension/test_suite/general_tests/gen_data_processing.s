b34a4e50ed6fae0ba1274eeb4978bcca78a7100b
Ryroro <stardust007@126.com>
2018-06-04 20:09:10 +0100

    add generated testcases

--- src/emulate_extension/test_suite/general_tests/gen_data_processing.s
+++ src/emulate_extension/test_suite/general_tests/gen_data_processing.s
@@ -1,200 +1,201 @@
-cmnpl r5, asr #170
-eorhis r9, asr #53
-andcss r13, #64
-cmnmi r3, lsl #93
-tstcs r9, lsl #192
-addpls r6, lsl r7
-mvncc r11, r5, ror r3
-adcvs r12, asr #16
-eorls r0, lsr #75
-movvss r8, r7, #97
-adclss r7, ror #120
-suble r11, asr #148
-cmncc r7, ror #105
-adcles r3, asr #136
-adclts r8, asr #137
-subcss r10, asr #36
-teqle r5, asr r6
-cmpcc r6, ror r11
-adcles r11, #88
-submis r7, lsr r11
-andge r2, #11
-rscvcs r2, asr #89
-adc r10, ror #138
-eorcss r7, lsl #92
-cmnlt r8, #34
-tstpl r12, asr #46
-cmneq r3, lsr #51
-mvnal r7, r8, #155
-andhis r13, #151
-mvnlss r9, r9, #153
-sbcvs r0, ror #164
-andals r7, #46
-teq r9, #16
-teqls r6, lsl #183
-subcs r0, lsr #36
-cmple r13, asr #142
-rsbvss r9, lsl #157
-sbcpl r6, #13
-mvnlts r8, r12, asr #181
-sbcges r4, #83
-cmplt r4, #36
-teqpl r7, #168
-cmnle r3, lsl r2
-suble r2, lsl r0
-adcals r6, ror #158
-cmpal r2, asr #34
-teqhi r7, lsr #39
-cmpvc r4, #70
-tstvs r1, lsl #101
-andlts r2, lsr r2
-tstmi r4, asr #88
-addals r4, #178
-teqge r4, lsr #71
-eornes r6, #188
-adcvc r9, lsr r2
-cmpvs r5, lsr r3
-rscvcs r13, #39
-rsclts r8, #5
-cmpgt r0, asr r7
-movvc r5, r8, lsl #74
-teqvs r4, #60
-rsblss r13, ror #128
-andccs r5, #14
-teqlt r7, lsl #163
-tsteq r0, #192
-adcge r7, #158
-rsbcc r4, ror r11
-cmnpl r6, lsl r12
-cmncc r1, ror r9
-adcccs r3, #69
-rsceqs r6, lsl #172
-tstls r10, asr #177
-andhi r8, asr #94
-sbcpl r11, asr #12
-tstpl r8, asr #65
-cmpge r7, asr #161
-tstvs r1, lsl #37
-adcvs r2, asr #47
-andals r8, lsl #46
-addals r1, #93
-cmnpl r5, lsl #111
-mvnals r11, r6, lsl #53
-sbcpl r5, #120
-cmnge r9, asr #57
-rscals r7, #55
-eorge r0, lsr #128
-teqgt r12, #17
-cmpgt r0, lsl #98
-rsbgts r4, #148
-rscvs r1, lsr #11
-andmis r9, lsl #164
-rsclss r4, asr #34
-addgts r0, #17
-movlts r9, r0, #109
-cmncs r1, ror r8
-teqls r4, #46
-cmpls r13, ror #178
-movcs r0, r7, #169
-addpls r6, asr #190
-adcge r0, asr r4
-subal r2, #66
-cmnvc r1, asr #98
-cmplt r1, lsl #86
-tstge r8, ror #198
-rscle r7, #10
-subvs r10, asr #148
-tstne r4, #121
-eorlss r4, #11
-andmis r7, #183
-andcs r13, lsl r12
-andcc r3, lsr r3
-subgts r1, ror r2
-tstle r3, lsl r3
-mvnls r8, r5, lsr r3
-addvss r3, lsr #187
-sbcvcs r7, #148
-cmpcc r1, #187
-rscvs r11, asr r10
-tstge r12, ror #99
-movals r13, r10, asr r12
-rsble r10, lsr #118
-tstvs r8, lsl #76
-movcs r9, r13, #125
-cmnne r7, #113
-andne r7, asr #93
-sbcals r11, asr #154
-sbcnes r6, ror #70
-cmpcc r6, ror #54
-cmneq r10, #59
-mvnpls r11, r8, asr #146
-sublss r10, asr #23
-sbcvs r1, #74
-cmpvs r3, #80
-rsbnes r3, asr r3
-adcgts r13, #147
-andvs r7, #143
-sbcals r3, lsr #188
-movlts r4, r11, lsl #22
-tst r1, #187
-movvcs r8, r10, #177
-movlss r4, r0, lsr #153
-eorne r6, #96
-addlts r3, ror #142
-rsccs r13, #104
-teqmi r8, #20
-rsbals r10, lsl #16
-mvnlss r11, r1, asr r2
-cmnne r5, asr r10
-subles r9, asr #31
-rscs r7, asr #106
-sbcges r1, asr #167
-cmpal r10, #58
-movls r12, r7, #50
-sbcne r8, #25
-andlts r0, asr #11
-rsc r12, ror #160
-addeq r10, lsl r12
-cmplt r1, asr #196
-subges r3, lsr #81
-teqal r1, asr r2
-mvns r5, r3, #139
-addlt r0, asr r12
-cmpcs r4, lsr #67
-mvnle r7, r13, ror #73
-rsble r5, lsr #187
-rscvcs r13, asr #189
-movpls r9, r8, asr #94
-cmncc r9, ror #59
-eoral r10, #129
-rsblts r0, asr #176
-cmppl r12, asr r3
-cmpmi r13, #167
-cmnne r9, asr #104
-addccs r13, asr #28
-sbceqs r6, #49
-tstls r2, #74
-andvcs r5, ror #119
-movlts r5, r5, asr r8
-mvngts r6, r11, asr r7
-eorlss r8, ror #152
-adcle r7, ror #23
-eorvss r2, #65
-adc r5, ror r10
-mvnvss r4, r11, #125
-rsbmis r0, ror #114
-tstvc r5, asr #135
-adccss r8, #76
-cmpgt r6, lsl r4
-rsbgts r9, #181
-rscals r11, lsl #121
-mvns r9, r0, lsr #6
-rsbal r13, #1
-rscgts r7, lsl r4
-teqvc r4, lsr #110
-subccs r4, lsr #105
-rscgts r3, #192
-mvnlts r2, r10, #30
-andlts r11, asr r1
-addals r5, ror #141
-tstle r4, asr #64
+teq r0, r5, lsl r7
+cmn r6, #46
+adc r10, r5, #169
+adc r3, r9, #153
+adc r11, r1, r11, lsr r6
+cmp r4, #105
+mov r4, #39
+rsb r10, r1, r9, lsl r0
+cmp r8, #44
+rsc r12, r0, #25
+eor r1, r9, r2, lsr r10
+teq r10, r5, lsl r3
+cmp r1, r6, asr r5
+bic r8, r12, #75
+adc r2, r13, #88
+tst r9, r6, asr r0
+and r8, r8, #138
+sub r5, r6, #148
+rsb r12, r6, #143
+eor r3, r13, r12, lsr r5
+rsb r3, r12, r11, lsl r5
+cmp r12, #17
+bic r3, r5, #138
+sbc r1, r2, r0, lsl r7
+rsb r13, r3, #182
+add r0, r8, #12
+mov r2, r8, lsr r12
+eor r11, r11, r13, asr r9
+bic r7, r7, r13, lsl r5
+orr r4, r0, r0, ror r8
+mvn r0, r4, lsl r9
+rsc r4, r6, r1, asr r12
+orr r8, r13, r13, asr r8
+eor r4, r1, #55
+cmn r5, r2, ror r7
+rsc r1, r13, #177
+eor r2, r4, r2, asr r0
+bic r7, r10, r3, ror r10
+rsb r4, r5, r4, ror r4
+bic r1, r9, #6
+sbc r13, r0, r9, asr r1
+tst r7, r12, asr r7
+cmn r11, r4, lsr r13
+rsc r2, r4, r3, asr r3
+bic r13, r8, r1, ror r11
+and r0, r6, r4, lsr r13
+mvn r3, r12, ror r3
+adc r3, r8, r13, ror r8
+cmp r4, r13, lsr r2
+rsb r7, r4, r7, ror r10
+orr r1, r1, r5, lsl r4
+bic r13, r1, r6, lsr r11
+eor r13, r4, #91
+orr r7, r0, r10, asr r8
+mvn r13, r3, asr r11
+adc r2, r5, #32
+and r1, r13, r10, ror r11
+mvn r9, #110
+rsc r9, r7, r10, asr r11
+cmn r13, r10, ror r8
+sbc r13, r3, #106
+sbc r10, r0, r4, lsl r9
+orr r5, r0, r5, ror r11
+bic r9, r11, r8, asr r13
+and r2, r13, r4, lsr r1
+add r2, r10, r6, lsl r1
+add r0, r13, #186
+rsc r9, r12, r10, lsr r12
+cmp r12, #117
+cmp r12, #41
+and r12, r13, r10, lsr r13
+sbc r5, r13, r4, ror r11
+tst r9, r7, lsr r13
+and r6, r7, r0, lsr r4
+cmp r6, r8, lsr r7
+sub r11, r12, r1, asr r1
+mvn r6, r8, lsl r11
+bic r8, r6, r7, asr r0
+rsb r8, r12, #55
+rsc r4, r9, r9, asr r0
+add r6, r13, r8, ror r1
+tst r8, r4, asr r0
+rsc r12, r7, #54
+orr r4, r11, r4, asr r12
+add r3, r2, #99
+and r0, r10, #195
+adc r10, r11, r9, lsl r6
+add r7, r0, r7, lsr r13
+orr r5, r4, r11, asr r7
+adc r12, r9, #101
+bic r3, r5, r1, asr r9
+sub r5, r6, r1, ror r1
+tst r10, r0, asr r7
+add r8, r4, r9, asr r13
+sbc r8, r1, #125
+tst r4, r13, asr r4
+cmp r0, #114
+adc r2, r7, r9, ror r10
+and r3, r0, #104
+rsb r7, r11, r6, asr r7
+rsb r0, r0, r1, lsr r2
+rsc r7, r6, #48
+and r7, r13, #188
+orr r13, r4, r2, lsl r2
+sbc r8, r12, r0, ror r3
+cmp r0, r8, asr r5
+bic r7, r6, r9, ror r6
+bic r0, r8, #56
+and r1, r8, r7, ror r3
+add r12, r7, #29
+sub r10, r2, r5, lsl r8
+sbc r1, r10, r11, lsl r4
+cmp r8, #68
+orr r10, r4, r1, ror r13
+mov r13, r3, lsr r8
+rsc r0, r10, r1, asr r6
+mov r2, r1, ror r13
+bic r3, r12, r2, ror r2
+rsb r0, r3, #164
+adc r5, r1, r12, lsl r4
+rsb r10, r7, #131
+adc r0, r9, r12, lsr r3
+adc r2, r4, #75
+and r7, r7, r11, asr r13
+sub r10, r1, r6, asr r3
+cmp r7, r10, lsl r8
+rsb r5, r3, r3, lsl r1
+sub r12, r10, r13, asr r4
+tst r12, r11, lsl r11
+eor r6, r1, r5, asr r11
+teq r1, #94
+adc r11, r13, r5, asr r10
+add r5, r1, r8, asr r10
+mov r2, r9, asr r12
+cmn r6, r4, lsr r10
+cmp r6, r0, asr r7
+add r8, r9, r4, asr r10
+adc r2, r1, r4, lsl r8
+sbc r6, r2, r4, lsr r7
+mvn r3, r4, asr r8
+mov r13, r2, lsr r6
+tst r4, r13, lsr r7
+add r1, r12, r7, lsl r3
+and r13, r2, r10, asr r3
+sub r4, r7, r3, ror r5
+adc r5, r11, #120
+tst r13, #188
+rsc r2, r11, #127
+rsb r2, r6, r13, ror r1
+teq r3, #117
+and r8, r12, #1
+sub r4, r4, r12, ror r0
+rsb r9, r6, r11, asr r0
+cmn r2, #178
+sbc r2, r1, r9, lsl r2
+rsb r4, r6, r1, asr r1
+add r6, r4, r4, lsr r8
+and r5, r7, r11, asr r8
+cmn r11, r1, lsl r6
+mvn r3, r9, asr r11
+sbc r13, r5, r2, ror r1
+mvn r8, r1, ror r10
+sbc r4, r3, r0, asr r7
+tst r10, r11, asr r10
+mvn r13, r9, lsr r10
+rsb r2, r6, r8, asr r5
+eor r4, r6, r4, ror r7
+adc r3, r0, #167
+mov r4, r8, asr r10
+sub r9, r7, r10, asr r2
+tst r5, #133
+cmp r11, r6, lsl r5
+tst r13, #59
+rsc r12, r9, r1, asr r5
+mov r2, r9, ror r6
+rsb r13, r3, #58
+rsc r0, r5, #163
+orr r12, r9, r7, lsr r6
+tst r3, r12, lsl r12
+adc r3, r0, r13, asr r12
+sbc r4, r8, #26
+tst r11, r5, lsl r1
+adc r3, r1, #158
+sub r9, r8, #166
+mov r9, r10, ror r10
+rsb r7, r11, #81
+rsc r0, r3, #199
+tst r2, r5, asr r11
+teq r7, r8, asr r4
+and r11, r6, #192
+rsb r5, r11, #142
+sub r6, r5, r11, lsr r6
+cmp r9, r9, asr r0
+cmp r9, r11, lsl r6
+cmp r0, r9, asr r13
+eor r0, r12, #167
+tst r1, #155
+rsb r10, r0, r10, lsl r4
+tst r9, r3, ror r7
+add r5, r2, r10, lsl r7
+
