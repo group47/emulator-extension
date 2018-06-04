40bde5a2463866f34f0701bf364768ac546b3ed5
Francis Nixon <fpn17@imperial.ac.uk>
2018-06-04 14:44:47 +0100

    syntax fix.

--- src/emulate_extension/test_suite/general_tests/combined_examples_from_spec_adc.s
+++ src/emulate_extension/test_suite/general_tests/combined_examples_from_spec_adc.s
@@ -1,200 +1,200 @@
-adc r1, r1 #1
-adc r1, r1 #5
-adc r1, r1 #9
-adc r1, r1 #15
-adc r1, r1 #2596896
-adc r2, r1, r1 lsl #2
-adc r2, r1, r1 lsl #3
-adc r2, r1, r1 lsl #5
-adc r2, r1, r1 lsl #9546355
-adc r2, r1, r1 lsr #2
-adc r2, r1, r1 lsr #3
-adc r2, r1, r1 lsr #5
-adc r2, r1, r1 lsr #9546355
-adc r2, r1, r1 asr #2
-adc r2, r1, r1 asr #3
-adc r2, r1, r1 asr #5
-adc r2, r1, r1 asr #9546355
-adc r3, pc #9546355
-adcs r1, r1 #1
-adcs r1, r1 #5
-adcs r1, r1 #9
-adcs r1, r1 #15
-adcs r1, r1 #2596896
-adcs r2, r1, r1 lsl #2
-adcs r2, r1, r1 lsl #3
-adcs r2, r1, r1 lsl #5
-adcs r2, r1, r1 lsl #9546355
-adcs r2, r1, r1 lsr #2
-adcs r2, r1, r1 lsr #3
-adcs r2, r1, r1 lsr #5
-adcs r2, r1, r1 lsr #9546355
-adcs r2, r1, r1 asr #2
-adcs r2, r1, r1 asr #3
-adcs r2, r1, r1 asr #5
-adcs r2, r1, r1 asr #9546355
-adcs r3, pc #9546355
-adceq r1, r1 #1
-adceq r1, r1 #5
-adceq r1, r1 #9
-adceq r1, r1 #15
-adceq r1, r1 #2596896
-adceq r2, r1, r1 lsl #2
-adceq r2, r1, r1 lsl #3
-adceq r2, r1, r1 lsl #5
-adceq r2, r1, r1 lsl #9546355
-adceq r2, r1, r1 lsr #2
-adceq r2, r1, r1 lsr #3
-adceq r2, r1, r1 lsr #5
-adceq r2, r1, r1 lsr #9546355
-adceq r2, r1, r1 asr #2
-adceq r2, r1, r1 asr #3
-adceq r2, r1, r1 asr #5
-adceq r2, r1, r1 asr #9546355
-adceq r3, pc #9546355
-adceqs r1, r1 #1
-adceqs r1, r1 #5
-adceqs r1, r1 #9
-adceqs r1, r1 #15
-adceqs r1, r1 #2596896
-adceqs r2, r1, r1 lsl #2
-adceqs r2, r1, r1 lsl #3
-adceqs r2, r1, r1 lsl #5
-adceqs r2, r1, r1 lsl #9546355
-adceqs r2, r1, r1 lsr #2
-adceqs r2, r1, r1 lsr #3
-adceqs r2, r1, r1 lsr #5
-adceqs r2, r1, r1 lsr #9546355
-adceqs r2, r1, r1 asr #2
-adceqs r2, r1, r1 asr #3
-adceqs r2, r1, r1 asr #5
-adceqs r2, r1, r1 asr #9546355
-adceqs r3, pc #1
-adcmi r1, r1 #1
-adcmi r1, r1 #5
-adcmi r1, r1 #9
-adcmi r1, r1 #15
-adcmi r1, r1 #2596896
-adcmi r2, r1, r1 lsl #2
-adcmi r2, r1, r1 lsl #3
-adcmi r2, r1, r1 lsl #5
-adcmi r2, r1, r1 lsl #9546355
-adcmi r2, r1, r1 lsr #2
-adcmi r2, r1, r1 lsr #3
-adcmi r2, r1, r1 lsr #5
-adcmi r2, r1, r1 lsr #9546355
-adcmi r2, r1, r1 asr #2
-adcmi r2, r1, r1 asr #3
-adcmi r2, r1, r1 asr #5
-adcmi r2, r1, r1 asr #9546355
-adcmi r3, pc #9546355
-adcmis r1, r1 #1
-adcmis r1, r1 #5
-adcmis r1, r1 #9
-adcmis r1, r1 #15
-adcmis r1, r1 #2596896
-adcmis r2, r1, r1 lsl #2
-adcmis r2, r1, r1 lsl #3
-adcmis r2, r1, r1 lsl #5
-adcmis r2, r1, r1 lsl #9546355
-adcmis r2, r1, r1 lsr #2
-adcmis r2, r1, r1 lsr #3
-adcmis r2, r1, r1 lsr #5
-adcmis r2, r1, r1 lsr #9546355
-adcmis r2, r1, r1 asr #2
-adcmis r2, r1, r1 asr #3
-adcmis r2, r1, r1 asr #5
-adcmis r2, r1, r1 asr #9546355
-adcmis r3, pc #1
-adcvs r1, r1 #1
-adcls r1, r1 #1
-adcvs r1, r1 #5
-adcls r1, r1 #5
-adcvs r1, r1 #9
-adcls r1, r1 #9
-adcvs r1, r1 #15
-adcls r1, r1 #15
-adcvs r1, r1 #2596896
-adcls r1, r1 #2596896
-adcvs r2, r1, r1 lsl #2
-adcls r2, r1, r1 lsl #2
-adcvs r2, r1, r1 lsl #3
-adcls r2, r1, r1 lsl #3
-adcvs r2, r1, r1 lsl #5
-adcls r2, r1, r1 lsl #5
-adcvs r2, r1, r1 lsl #9546355
-adcls r2, r1, r1 lsl #9546355
-adcvs r2, r1, r1 lsr #2
-adcls r2, r1, r1 lsr #2
-adcvs r2, r1, r1 lsr #3
-adcls r2, r1, r1 lsr #3
-adcvs r2, r1, r1 lsr #5
-adcls r2, r1, r1 lsr #5
-adcvs r2, r1, r1 lsr #9546355
-adcls r2, r1, r1 lsr #9546355
-adcvs r2, r1, r1 asr #2
-adcls r2, r1, r1 asr #2
-adcvs r2, r1, r1 asr #3
-adcls r2, r1, r1 asr #3
-adcvs r2, r1, r1 asr #5
-adcls r2, r1, r1 asr #5
-adcvs r2, r1, r1 asr #9546355
-adcls r2, r1, r1 asr #9546355
-adcvs r3, pc #9546355
-adcls r3, pc #9546355
-adcvss r1, r1 #1
-adclss r1, r1 #1
-adcvss r1, r1 #5
-adclss r1, r1 #5
-adcvss r1, r1 #9
-adclss r1, r1 #9
-adcvss r1, r1 #15
-adclss r1, r1 #15
-adcvss r1, r1 #2596896
-adclss r1, r1 #2596896
-adcvss r2, r1, r1 lsl #2
-adclss r2, r1, r1 lsl #2
-adcvss r2, r1, r1 lsl #3
-adclss r2, r1, r1 lsl #3
-adcvss r2, r1, r1 lsl #5
-adclss r2, r1, r1 lsl #5
-adcvss r2, r1, r1 lsl #9546355
-adclss r2, r1, r1 lsl #9546355
-adcvss r2, r1, r1 lsr #2
-adclss r2, r1, r1 lsr #2
-adcvss r2, r1, r1 lsr #3
-adclss r2, r1, r1 lsr #3
-adcvss r2, r1, r1 lsr #5
-adclss r2, r1, r1 lsr #5
-adcvss r2, r1, r1 lsr #9546355
-adclss r2, r1, r1 lsr #9546355
-adcvss r2, r1, r1 asr #2
-adclss r2, r1, r1 asr #2
-adcvss r2, r1, r1 asr #3
-adclss r2, r1, r1 asr #3
-adcvss r2, r1, r1 asr #5
-adclss r2, r1, r1 asr #5
-adcvss r2, r1, r1 asr #9546355
-adclss r2, r1, r1 asr #9546355
-adcvss r3, pc #1
-adclss r3, pc #1
-adcvcs r2, r1, r1 lsr #9546355
-adchis r2, r1, r1 lsr #9546355
-adcges r2, r1, r1 lsr #9546355
-adclts r2, r1, r1 lsr #9546355
-adcles r2, r1, r1 lsr #9546355
-adcmis r2, r1, r1 lsr #9546355
-adcpls r2, r1, r1 lsr #9546355
-adclos r2, r1, r1 lsr #9546355
-adcvcs r2, r1, r1 lsr #9546355
-adchis r2, r1, r1 lsr #9546355
-adcges r2, r1, r1 lsr #9546355
-adclts r2, r1, r1 lsr #9546355
-adcles r2, r1, r1 lsr #9546355
-adcmis r2, r1, r1 lsr #9546355
-adcpls r2, r1, r1 lsr #9546355
-adclos r2, r1, r1 lsr #9546355
-adcvcs r2, r1, r1 lsr #9546355
-adchis r2, r1, r1 lsr #9546355
-adcges r2, r1, r1 lsr #9546355
-adclts r2, r1, r1 lsr #9546355
+adc r1, r1, #1
+adc r1, r1, #5
+adc r1, r1, #9
+adc r1, r1, #15
+adc r1, r1, #2596896
+adc r2, r1, r1 ,lsl #2
+adc r2, r1, r1 ,lsl #3
+adc r2, r1, r1 ,lsl #5
+adc r2, r1, r1 ,lsl #9546355
+adc r2, r1, r1 ,lsr #2
+adc r2, r1, r1 ,lsr #3
+adc r2, r1, r1 ,lsr #5
+adc r2, r1, r1 ,lsr #9546355
+adc r2, r1, r1 ,asr #2
+adc r2, r1, r1 ,asr #3
+adc r2, r1, r1 ,asr #5
+adc r2, r1, r1 ,asr #9546355
+adc r3, pc, #9546355
+adcs r1, r1, #1
+adcs r1, r1, #5
+adcs r1, r1, #9
+adcs r1, r1, #15
+adcs r1, r1, #2596896
+adcs r2, r1, r1 ,lsl #2
+adcs r2, r1, r1 ,lsl #3
+adcs r2, r1, r1 ,lsl #5
+adcs r2, r1, r1 ,lsl #9546355
+adcs r2, r1, r1 ,lsr #2
+adcs r2, r1, r1 ,lsr #3
+adcs r2, r1, r1 ,lsr #5
+adcs r2, r1, r1 ,lsr #9546355
+adcs r2, r1, r1 ,asr #2
+adcs r2, r1, r1 ,asr #3
+adcs r2, r1, r1 ,asr #5
+adcs r2, r1, r1 ,asr #9546355
+adcs r3, pc, #9546355
+adceq r1, r1, #1
+adceq r1, r1, #5
+adceq r1, r1, #9
+adceq r1, r1, #15
+adceq r1, r1, #2596896
+adceq r2, r1, r1 ,lsl #2
+adceq r2, r1, r1 ,lsl #3
+adceq r2, r1, r1 ,lsl #5
+adceq r2, r1, r1 ,lsl #9546355
+adceq r2, r1, r1 ,lsr #2
+adceq r2, r1, r1 ,lsr #3
+adceq r2, r1, r1 ,lsr #5
+adceq r2, r1, r1 ,lsr #9546355
+adceq r2, r1, r1 ,asr #2
+adceq r2, r1, r1 ,asr #3
+adceq r2, r1, r1 ,asr #5
+adceq r2, r1, r1 ,asr #9546355
+adceq r3, pc, #9546355
+adceqs r1, r1, #1
+adceqs r1, r1, #5
+adceqs r1, r1, #9
+adceqs r1, r1, #15
+adceqs r1, r1, #2596896
+adceqs r2, r1, r1 ,lsl #2
+adceqs r2, r1, r1 ,lsl #3
+adceqs r2, r1, r1 ,lsl #5
+adceqs r2, r1, r1 ,lsl #9546355
+adceqs r2, r1, r1 ,lsr #2
+adceqs r2, r1, r1 ,lsr #3
+adceqs r2, r1, r1 ,lsr #5
+adceqs r2, r1, r1 ,lsr #9546355
+adceqs r2, r1, r1 ,asr #2
+adceqs r2, r1, r1 ,asr #3
+adceqs r2, r1, r1 ,asr #5
+adceqs r2, r1, r1 ,asr #9546355
+adceqs r3, pc, #1
+adcmi r1, r1, #1
+adcmi r1, r1, #5
+adcmi r1, r1, #9
+adcmi r1, r1, #15
+adcmi r1, r1, #2596896
+adcmi r2, r1, r1 ,lsl #2
+adcmi r2, r1, r1 ,lsl #3
+adcmi r2, r1, r1 ,lsl #5
+adcmi r2, r1, r1 ,lsl #9546355
+adcmi r2, r1, r1 ,lsr #2
+adcmi r2, r1, r1 ,lsr #3
+adcmi r2, r1, r1 ,lsr #5
+adcmi r2, r1, r1 ,lsr #9546355
+adcmi r2, r1, r1 ,asr #2
+adcmi r2, r1, r1 ,asr #3
+adcmi r2, r1, r1 ,asr #5
+adcmi r2, r1, r1 ,asr #9546355
+adcmi r3, pc, #9546355
+adcmis r1, r1, #1
+adcmis r1, r1, #5
+adcmis r1, r1, #9
+adcmis r1, r1, #15
+adcmis r1, r1, #2596896
+adcmis r2, r1, r1 ,lsl #2
+adcmis r2, r1, r1 ,lsl #3
+adcmis r2, r1, r1 ,lsl #5
+adcmis r2, r1, r1 ,lsl #9546355
+adcmis r2, r1, r1 ,lsr #2
+adcmis r2, r1, r1 ,lsr #3
+adcmis r2, r1, r1 ,lsr #5
+adcmis r2, r1, r1 ,lsr #9546355
+adcmis r2, r1, r1 ,asr #2
+adcmis r2, r1, r1 ,asr #3
+adcmis r2, r1, r1 ,asr #5
+adcmis r2, r1, r1 ,asr #9546355
+adcmis r3, pc, #1
+adcvs r1, r1, #1
+adcls r1, r1, #1
+adcvs r1, r1, #5
+adcls r1, r1, #5
+adcvs r1, r1, #9
+adcls r1, r1, #9
+adcvs r1, r1, #15
+adcls r1, r1, #15
+adcvs r1, r1, #2596896
+adcls r1, r1, #2596896
+adcvs r2, r1, r1 ,lsl #2
+adcls r2, r1, r1 ,lsl #2
+adcvs r2, r1, r1 ,lsl #3
+adcls r2, r1, r1 ,lsl #3
+adcvs r2, r1, r1 ,lsl #5
+adcls r2, r1, r1 ,lsl #5
+adcvs r2, r1, r1 ,lsl #9546355
+adcls r2, r1, r1 ,lsl #9546355
+adcvs r2, r1, r1 ,lsr #2
+adcls r2, r1, r1 ,lsr #2
+adcvs r2, r1, r1 ,lsr #3
+adcls r2, r1, r1 ,lsr #3
+adcvs r2, r1, r1 ,lsr #5
+adcls r2, r1, r1 ,lsr #5
+adcvs r2, r1, r1 ,lsr #9546355
+adcls r2, r1, r1 ,lsr #9546355
+adcvs r2, r1, r1 ,asr #2
+adcls r2, r1, r1 ,asr #2
+adcvs r2, r1, r1 ,asr #3
+adcls r2, r1, r1 ,asr #3
+adcvs r2, r1, r1 ,asr #5
+adcls r2, r1, r1 ,asr #5
+adcvs r2, r1, r1 ,asr #9546355
+adcls r2, r1, r1 ,asr #9546355
+adcvs r3, pc, #9546355
+adcls r3, pc, #9546355
+adcvss r1, r1, #1
+adclss r1, r1, #1
+adcvss r1, r1, #5
+adclss r1, r1, #5
+adcvss r1, r1, #9
+adclss r1, r1, #9
+adcvss r1, r1, #15
+adclss r1, r1, #15
+adcvss r1, r1, #2596896
+adclss r1, r1, #2596896
+adcvss r2, r1, r1 ,lsl #2
+adclss r2, r1, r1 ,lsl #2
+adcvss r2, r1, r1 ,lsl #3
+adclss r2, r1, r1 ,lsl #3
+adcvss r2, r1, r1 ,lsl #5
+adclss r2, r1, r1 ,lsl #5
+adcvss r2, r1, r1 ,lsl #9546355
+adclss r2, r1, r1 ,lsl #9546355
+adcvss r2, r1, r1 ,lsr #2
+adclss r2, r1, r1 ,lsr #2
+adcvss r2, r1, r1 ,lsr #3
+adclss r2, r1, r1 ,lsr #3
+adcvss r2, r1, r1 ,lsr #5
+adclss r2, r1, r1 ,lsr #5
+adcvss r2, r1, r1 ,lsr #9546355
+adclss r2, r1, r1 ,lsr #9546355
+adcvss r2, r1, r1 ,asr #2
+adclss r2, r1, r1 ,asr #2
+adcvss r2, r1, r1 ,asr #3
+adclss r2, r1, r1 ,asr #3
+adcvss r2, r1, r1 ,asr #5
+adclss r2, r1, r1 ,asr #5
+adcvss r2, r1, r1 ,asr #9546355
+adclss r2, r1, r1 ,asr #9546355
+adcvss r3, pc, #1
+adclss r3, pc, #1
+adcvcs r2, r1, r1 ,lsr #9546355
+adchis r2, r1, r1 ,lsr #9546355
+adcges r2, r1, r1 ,lsr #9546355
+adclts r2, r1, r1 ,lsr #9546355
+adcles r2, r1, r1 ,lsr #9546355
+adcmis r2, r1, r1 ,lsr #9546355
+adcpls r2, r1, r1 ,lsr #9546355
+adclos r2, r1, r1 ,lsr #9546355
+adcvcs r2, r1, r1 ,lsr #9546355
+adchis r2, r1, r1 ,lsr #9546355
+adcges r2, r1, r1 ,lsr #9546355
+adclts r2, r1, r1 ,lsr #9546355
+adcles r2, r1, r1 ,lsr #9546355
+adcmis r2, r1, r1 ,lsr #9546355
+adcpls r2, r1, r1 ,lsr #9546355
+adclos r2, r1, r1 ,lsr #9546355
+adcvcs r2, r1, r1 ,lsr #9546355
+adchis r2, r1, r1 ,lsr #9546355
+adcges r2, r1, r1 ,lsr #9546355
+adclts r2, r1, r1 ,lsr #9546355
