61f1ad429ffaf66c61d8b8d5e087d02ea0bb5ac4
Ryroro <stardust007@126.com>
2018-06-04 20:46:25 +0100

    add al data processing

---
+++ src/emulate_extension/test_suite/general_tests/gen_data_processing_al.s
@@ -0,0 +1,200 @@
+tstal r9, #122
+orral r5, r4, r5, lsr r6
+orrals r4, r1, r12, lsl r8
+cmpal r5, r2, asr r6
+cmpal r4, r12, lsl r9
+movals r9, #61
+cmpal r5, #2
+subal r10, r10, #148
+andal r9, r9, r9, asr r0
+teqal r0, r5, lsl r2
+orrcss r10, r12, #52
+cmn r7, #68
+tstal r2, r5, ror r9
+cmp r4, r9, asr r3
+rscmis r1, r11, #8
+cmpcc r2, #60
+cmnle r9, r10, lsl r8
+tstpl r6, #155
+orrs r8, r6, #103
+addccs r4, r11, #19
+cmpal r11, r7, lsr r4
+andnes r1, r1, r10, asr r1
+rsbgt r6, r5, r5, asr r4
+sbcals r11, r6, #86
+eorgt r5, r1, #192
+andvcs r12, r6, r3, lsl r3
+bicmis r12, r4, r12, asr r8
+rscccs r2, r5, #27
+rschi r0, r7, r3, ror r1
+bicvss r9, r0, r6, asr r7
+addhis r10, r5, r8, ror r12
+adcpls r4, r9, r5, ror r8
+movpls r4, r8, lsr r1
+bicges r9, r1, #125
+sbcvss r10, r5, #165
+tstcs r8, r12, asr r5
+orrmis r12, r7, r9, lsl r0
+mvn r5, r9, lsl r9
+adcmi r2, r12, r11, ror r9
+addle r11, r7, r6, asr r8
+rscls r5, r6, r3, lsr r6
+rsbeq r8, r1, #160
+adccss r2, r3, r8, asr r0
+rscvs r2, r6, r0, asr r10
+biclt r12, r2, r6, asr r1
+movles r0, #18
+adcges r1, r2, #195
+cmpgt r2, #40
+subcs r3, r6, #82
+cmneq r0, r5, lsl r7
+bicle r5, r2, r5, lsl r4
+teqcs r0, r4, ror r12
+teqls r6, r5, asr r6
+bicle r9, r11, r4, asr r5
+adclts r4, r6, #58
+cmp r6, #18
+sbcvs r0, r12, #198
+sbcges r3, r0, r0, ror r0
+subcs r1, r0, r6, lsl r12
+cmplt r5, r1, lsr r3
+cmnal r7, r3, lsr r6
+tstlt r7, r11, lsr r2
+andvc r11, r10, r7, lsr r0
+cmncs r3, r8, lsl r8
+cmpvs r10, r12, ror r4
+sbcge r10, r1, r3, ror r0
+adccss r8, r7, r7, asr r5
+rsble r9, r5, r2, ror r0
+teq r11, r9, asr r6
+movvss r2, #75
+orrles r2, r6, r9, lsr r9
+suble r12, r5, #73
+adcmi r2, r10, r7, lsl r9
+eorpls r8, r11, r8, asr r5
+rsblss r10, r2, r6, lsr r11
+movs r7, #164
+tstal r3, r11, asr r12
+cmpvs r3, r11, ror r1
+andlss r10, r5, r0, ror r8
+mvnhis r0, r0, asr r1
+eornes r4, r7, #84
+cmpvc r0, r7, asr r11
+andmi r9, r6, #46
+teqne r7, r3, asr r6
+sbcgts r1, r6, r3, asr r9
+orrals r11, r2, #17
+cmnvc r2, r9, lsr r8
+sbcal r4, r4, r7, asr r12
+rscccs r1, r12, #80
+andnes r1, r10, #44
+addvcs r11, r0, r1, lsl r6
+sbcls r7, r11, r8, lsr r10
+sbcvc r3, r10, #195
+addges r4, r7, r3, ror r7
+bicvss r0, r6, #186
+tsthi r7, r9, lsr r3
+bicgt r11, r7, r8, ror r6
+subals r11, r2, r5, asr r6
+cmnpl r11, #185
+eorccs r4, r2, r11, ror r12
+movhis r11, r3, ror r0
+bichi r3, r9, r11, lsl r3
+teqvc r11, r1, asr r3
+eorcss r4, r2, r5, lsl r6
+tstne r2, r3, lsl r6
+andles r11, r3, r0, lsr r9
+orrlts r11, r4, r9, asr r3
+rscls r3, r1, #120
+movcc r11, r11, lsl r5
+orrlts r5, r7, r2, asr r6
+eorcs r2, r11, r9, ror r3
+tstcc r4, r4, ror r1
+eors r11, r3, r2, asr r6
+andvc r7, r7, #20
+subgt r8, r0, r12, ror r10
+movgt r4, r2, asr r8
+moveqs r6, r2, ror r4
+rscvcs r2, r7, r7, ror r11
+bicne r1, r7, r6, asr r3
+mvnccs r0, r1, asr r9
+mvnccs r11, #71
+rsceqs r12, r11, #125
+teqlt r1, #146
+sbcvc r10, r10, r11, lsl r3
+orrle r5, r9, r0, lsl r11
+andhis r12, r4, r3, asr r4
+adcvss r6, r12, #109
+bicge r0, r4, r1, asr r8
+tstvs r3, r11, ror r2
+rsceqs r11, r6, #94
+bicnes r4, r12, #144
+cmngt r12, r10, asr r1
+rsbne r0, r10, r7, asr r3
+orrgts r1, r11, r6, lsr r1
+sbcvc r12, r3, #5
+cmp r8, r4, asr r1
+mvnge r0, r5, lsl r0
+orrne r7, r12, r4, asr r9
+subpls r2, r9, r3, lsl r1
+cmpal r7, r12, asr r1
+subs r9, r11, r3, ror r2
+movmis r2, #141
+teqmi r3, #11
+movvss r11, #156
+addlss r3, r12, #73
+biclt r0, r2, r9, lsr r9
+sbcs r11, r10, #46
+mvnlss r12, r5, lsl r11
+cmpvc r3, r0, ror r6
+eorccs r9, r1, #131
+biceqs r8, r3, r8, ror r12
+subles r9, r7, r11, lsl r8
+adcges r9, r1, r5, lsl r3
+subgt r5, r4, #192
+orrges r7, r2, r5, asr r4
+rsble r7, r9, r9, ror r3
+movlts r12, r5, lsr r9
+adcges r6, r8, #130
+teqal r9, r11, ror r12
+tstvs r0, r9, lsr r6
+cmnne r8, r1, ror r12
+bicvs r6, r1, #157
+mvnhi r3, #99
+sbcs r2, r5, #73
+teqpl r3, r11, lsl r11
+tstmi r10, #133
+movlts r8, #42
+movgts r6, #170
+andgt r5, r7, #16
+rscne r8, r9, #178
+adcgt r3, r3, r3, asr r7
+eorvss r4, r3, r0, asr r11
+adccss r6, r9, #174
+rsceq r2, r5, #110
+addges r7, r0, #169
+cmple r2, r7, ror r11
+rsbgt r8, r7, #128
+cmpcc r12, #83
+orreq r9, r8, r8, ror r11
+cmpeq r3, r12, ror r9
+rscges r6, r10, r7, asr r12
+cmpls r8, r3, ror r12
+cmncs r0, r12, asr r8
+andlts r4, r11, r0, lsl r0
+rsbges r12, r9, r8, lsl r7
+addlts r7, r9, r4, asr r4
+tstmi r0, r5, ror r2
+rsbals r8, r7, r9, ror r7
+biclss r10, r11, r7, ror r8
+tstvs r11, r5, lsr r7
+adchi r9, r3, r7, asr r10
+andvss r3, r3, r11, lsr r5
+orrlts r8, r9, r12, asr r2
+orreqs r5, r12, r0, ror r11
+submis r5, r7, #139
+rscgts r2, r10, r4, asr r12
+cmpeq r3, r7, ror r9
+cmnle r5, r5, asr r9
+rsceqs r4, r8, r12, asr r7
+adcne r6, r11, r3, lsl r7
