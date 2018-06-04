300aded82db6840e858a443eaaf80cd79bb9575c
Francis Nixon <fpn17@imperial.ac.uk>
2018-06-04 14:38:30 +0100

    Added some general_test instructions

---
+++ src/emulate_extension/test_suite/general_tests/combined_examples_from_spec_add_sub_arm.s
@@ -0,0 +1,200 @@
+add r1, r1 #1
+add r1, r1 #5
+add r1, r1 #9
+add r1, r1 #15
+add r1, r1 #2596896
+add r2, r1, r1 lsl #2
+add r2, r1, r1 lsl #3
+add r2, r1, r1 lsl #5
+add r2, r1, r1 lsl #9546355
+add r2, r1, r1 lsr #2
+add r2, r1, r1 lsr #3
+add r2, r1, r1 lsr #5
+add r2, r1, r1 lsr #9546355
+add r2, r1, r1 asr #2
+add r2, r1, r1 asr #3
+add r2, r1, r1 asr #5
+add r2, r1, r1 asr #9546355
+add r3, pc #9546355
+sub r1, r1 #1
+sub r1, r1 #5
+sub r1, r1 #9
+sub r1, r1 #15
+sub r1, r1 #2596896
+sub r2, r1, r1 lsl #2
+sub r2, r1, r1 lsl #3
+sub r2, r1, r1 lsl #5
+sub r2, r1, r1 lsl #9546355
+sub r2, r1, r1 lsr #2
+sub r2, r1, r1 lsr #3
+sub r2, r1, r1 lsr #5
+sub r2, r1, r1 lsr #9546355
+sub r2, r1, r1 asr #2
+sub r2, r1, r1 asr #3
+sub r2, r1, r1 asr #5
+sub r2, r1, r1 asr #9546355
+sub r3, pc #9546355
+addeq r1, r1 #1
+addeq r1, r1 #5
+addeq r1, r1 #9
+addeq r1, r1 #15
+addeq r1, r1 #2596896
+addeq r2, r1, r1 lsl #2
+addeq r2, r1, r1 lsl #3
+addeq r2, r1, r1 lsl #5
+addeq r2, r1, r1 lsl #9546355
+addeq r2, r1, r1 lsr #2
+addeq r2, r1, r1 lsr #3
+addeq r2, r1, r1 lsr #5
+addeq r2, r1, r1 lsr #9546355
+addeq r2, r1, r1 asr #2
+addeq r2, r1, r1 asr #3
+addeq r2, r1, r1 asr #5
+addeq r2, r1, r1 asr #9546355
+addeq r3, pc #9546355
+subeq r1, r1 #1
+subeq r1, r1 #5
+subeq r1, r1 #9
+subeq r1, r1 #15
+subeq r1, r1 #2596896
+subeq r2, r1, r1 lsl #2
+subeq r2, r1, r1 lsl #3
+subeq r2, r1, r1 lsl #5
+subeq r2, r1, r1 lsl #9546355
+subeq r2, r1, r1 lsr #2
+subeq r2, r1, r1 lsr #3
+subeq r2, r1, r1 lsr #5
+subeq r2, r1, r1 lsr #9546355
+subeq r2, r1, r1 asr #2
+subeq r2, r1, r1 asr #3
+subeq r2, r1, r1 asr #5
+subeq r2, r1, r1 asr #9546355
+subeq r3, pc #1
+addmi r1, r1 #1
+addmi r1, r1 #5
+addmi r1, r1 #9
+addmi r1, r1 #15
+addmi r1, r1 #2596896
+addmi r2, r1, r1 lsl #2
+addmi r2, r1, r1 lsl #3
+addmi r2, r1, r1 lsl #5
+addmi r2, r1, r1 lsl #9546355
+addmi r2, r1, r1 lsr #2
+addmi r2, r1, r1 lsr #3
+addmi r2, r1, r1 lsr #5
+addmi r2, r1, r1 lsr #9546355
+addmi r2, r1, r1 asr #2
+addmi r2, r1, r1 asr #3
+addmi r2, r1, r1 asr #5
+addmi r2, r1, r1 asr #9546355
+addmi r3, pc #9546355
+submi r1, r1 #1
+submi r1, r1 #5
+submi r1, r1 #9
+submi r1, r1 #15
+submi r1, r1 #2596896
+submi r2, r1, r1 lsl #2
+submi r2, r1, r1 lsl #3
+submi r2, r1, r1 lsl #5
+submi r2, r1, r1 lsl #9546355
+submi r2, r1, r1 lsr #2
+submi r2, r1, r1 lsr #3
+submi r2, r1, r1 lsr #5
+submi r2, r1, r1 lsr #9546355
+submi r2, r1, r1 asr #2
+submi r2, r1, r1 asr #3
+submi r2, r1, r1 asr #5
+submi r2, r1, r1 asr #9546355
+submi r3, pc #1
+addvs r1, r1 #1
+addls r1, r1 #1
+addvs r1, r1 #5
+addls r1, r1 #5
+addvs r1, r1 #9
+addls r1, r1 #9
+addvs r1, r1 #15
+addls r1, r1 #15
+addvs r1, r1 #2596896
+addls r1, r1 #2596896
+addvs r2, r1, r1 lsl #2
+addls r2, r1, r1 lsl #2
+addvs r2, r1, r1 lsl #3
+addls r2, r1, r1 lsl #3
+addvs r2, r1, r1 lsl #5
+addls r2, r1, r1 lsl #5
+addvs r2, r1, r1 lsl #9546355
+addls r2, r1, r1 lsl #9546355
+addvs r2, r1, r1 lsr #2
+addls r2, r1, r1 lsr #2
+addvs r2, r1, r1 lsr #3
+addls r2, r1, r1 lsr #3
+addvs r2, r1, r1 lsr #5
+addls r2, r1, r1 lsr #5
+addvs r2, r1, r1 lsr #9546355
+addls r2, r1, r1 lsr #9546355
+addvs r2, r1, r1 asr #2
+addls r2, r1, r1 asr #2
+addvs r2, r1, r1 asr #3
+addls r2, r1, r1 asr #3
+addvs r2, r1, r1 asr #5
+addls r2, r1, r1 asr #5
+addvs r2, r1, r1 asr #9546355
+addls r2, r1, r1 asr #9546355
+addvs r3, pc #9546355
+addls r3, pc #9546355
+subvs r1, r1 #1
+subls r1, r1 #1
+subvs r1, r1 #5
+subls r1, r1 #5
+subvs r1, r1 #9
+subls r1, r1 #9
+subvs r1, r1 #15
+subls r1, r1 #15
+subvs r1, r1 #2596896
+subls r1, r1 #2596896
+subvs r2, r1, r1 lsl #2
+subls r2, r1, r1 lsl #2
+subvs r2, r1, r1 lsl #3
+subls r2, r1, r1 lsl #3
+subvs r2, r1, r1 lsl #5
+subls r2, r1, r1 lsl #5
+subvs r2, r1, r1 lsl #9546355
+subls r2, r1, r1 lsl #9546355
+subvs r2, r1, r1 lsr #2
+subls r2, r1, r1 lsr #2
+subvs r2, r1, r1 lsr #3
+subls r2, r1, r1 lsr #3
+subvs r2, r1, r1 lsr #5
+subls r2, r1, r1 lsr #5
+subvs r2, r1, r1 lsr #9546355
+subls r2, r1, r1 lsr #9546355
+subvs r2, r1, r1 asr #2
+subls r2, r1, r1 asr #2
+subvs r2, r1, r1 asr #3
+subls r2, r1, r1 asr #3
+subvs r2, r1, r1 asr #5
+subls r2, r1, r1 asr #5
+subvs r2, r1, r1 asr #9546355
+subls r2, r1, r1 asr #9546355
+subvs r3, pc #1
+subls r3, pc #1
+subvc r2, r1, r1 lsr #9546355
+subhi r2, r1, r1 lsr #9546355
+subge r2, r1, r1 lsr #9546355
+sublt r2, r1, r1 lsr #9546355
+suble r2, r1, r1 lsr #9546355
+submi r2, r1, r1 lsr #9546355
+subpl r2, r1, r1 lsr #9546355
+sublo r2, r1, r1 lsr #9546355
+subvc r2, r1, r1 lsr #9546355
+subhi r2, r1, r1 lsr #9546355
+subge r2, r1, r1 lsr #9546355
+sublt r2, r1, r1 lsr #9546355
+suble r2, r1, r1 lsr #9546355
+submi r2, r1, r1 lsr #9546355
+subpl r2, r1, r1 lsr #9546355
+sublo r2, r1, r1 lsr #9546355
+subvc r2, r1, r1 lsr #9546355
+subhi r2, r1, r1 lsr #9546355
+subge r2, r1, r1 lsr #9546355
+sublt r2, r1, r1 lsr #9546355
---
+++ src/emulate_extension/test_suite/general_tests/combined_examples_from_spec_and.s
@@ -0,0 +1,200 @@
+and r1, r1 #1
+and r1, r1 #5
+and r1, r1 #9
+and r1, r1 #15
+and r1, r1 #2596896
+and r2, r1, r1 lsl #2
+and r2, r1, r1 lsl #3
+and r2, r1, r1 lsl #5
+and r2, r1, r1 lsl #9546355
+and r2, r1, r1 lsr #2
+and r2, r1, r1 lsr #3
+and r2, r1, r1 lsr #5
+and r2, r1, r1 lsr #9546355
+and r2, r1, r1 asr #2
+and r2, r1, r1 asr #3
+and r2, r1, r1 asr #5
+and r2, r1, r1 asr #9546355
+and r3, pc #9546355
+ands r1, r1 #1
+ands r1, r1 #5
+ands r1, r1 #9
+ands r1, r1 #15
+ands r1, r1 #2596896
+ands r2, r1, r1 lsl #2
+ands r2, r1, r1 lsl #3
+ands r2, r1, r1 lsl #5
+ands r2, r1, r1 lsl #9546355
+ands r2, r1, r1 lsr #2
+ands r2, r1, r1 lsr #3
+ands r2, r1, r1 lsr #5
+ands r2, r1, r1 lsr #9546355
+ands r2, r1, r1 asr #2
+ands r2, r1, r1 asr #3
+ands r2, r1, r1 asr #5
+ands r2, r1, r1 asr #9546355
+ands r3, pc #9546355
+andeq r1, r1 #1
+andeq r1, r1 #5
+andeq r1, r1 #9
+andeq r1, r1 #15
+andeq r1, r1 #2596896
+andeq r2, r1, r1 lsl #2
+andeq r2, r1, r1 lsl #3
+andeq r2, r1, r1 lsl #5
+andeq r2, r1, r1 lsl #9546355
+andeq r2, r1, r1 lsr #2
+andeq r2, r1, r1 lsr #3
+andeq r2, r1, r1 lsr #5
+andeq r2, r1, r1 lsr #9546355
+andeq r2, r1, r1 asr #2
+andeq r2, r1, r1 asr #3
+andeq r2, r1, r1 asr #5
+andeq r2, r1, r1 asr #9546355
+andeq r3, pc #9546355
+andeqs r1, r1 #1
+andeqs r1, r1 #5
+andeqs r1, r1 #9
+andeqs r1, r1 #15
+andeqs r1, r1 #2596896
+andeqs r2, r1, r1 lsl #2
+andeqs r2, r1, r1 lsl #3
+andeqs r2, r1, r1 lsl #5
+andeqs r2, r1, r1 lsl #9546355
+andeqs r2, r1, r1 lsr #2
+andeqs r2, r1, r1 lsr #3
+andeqs r2, r1, r1 lsr #5
+andeqs r2, r1, r1 lsr #9546355
+andeqs r2, r1, r1 asr #2
+andeqs r2, r1, r1 asr #3
+andeqs r2, r1, r1 asr #5
+andeqs r2, r1, r1 asr #9546355
+andeqs r3, pc #1
+andmi r1, r1 #1
+andmi r1, r1 #5
+andmi r1, r1 #9
+andmi r1, r1 #15
+andmi r1, r1 #2596896
+andmi r2, r1, r1 lsl #2
+andmi r2, r1, r1 lsl #3
+andmi r2, r1, r1 lsl #5
+andmi r2, r1, r1 lsl #9546355
+andmi r2, r1, r1 lsr #2
+andmi r2, r1, r1 lsr #3
+andmi r2, r1, r1 lsr #5
+andmi r2, r1, r1 lsr #9546355
+andmi r2, r1, r1 asr #2
+andmi r2, r1, r1 asr #3
+andmi r2, r1, r1 asr #5
+andmi r2, r1, r1 asr #9546355
+andmi r3, pc #9546355
+andmis r1, r1 #1
+andmis r1, r1 #5
+andmis r1, r1 #9
+andmis r1, r1 #15
+andmis r1, r1 #2596896
+andmis r2, r1, r1 lsl #2
+andmis r2, r1, r1 lsl #3
+andmis r2, r1, r1 lsl #5
+andmis r2, r1, r1 lsl #9546355
+andmis r2, r1, r1 lsr #2
+andmis r2, r1, r1 lsr #3
+andmis r2, r1, r1 lsr #5
+andmis r2, r1, r1 lsr #9546355
+andmis r2, r1, r1 asr #2
+andmis r2, r1, r1 asr #3
+andmis r2, r1, r1 asr #5
+andmis r2, r1, r1 asr #9546355
+andmis r3, pc #1
+andvs r1, r1 #1
+andls r1, r1 #1
+andvs r1, r1 #5
+andls r1, r1 #5
+andvs r1, r1 #9
+andls r1, r1 #9
+andvs r1, r1 #15
+andls r1, r1 #15
+andvs r1, r1 #2596896
+andls r1, r1 #2596896
+andvs r2, r1, r1 lsl #2
+andls r2, r1, r1 lsl #2
+andvs r2, r1, r1 lsl #3
+andls r2, r1, r1 lsl #3
+andvs r2, r1, r1 lsl #5
+andls r2, r1, r1 lsl #5
+andvs r2, r1, r1 lsl #9546355
+andls r2, r1, r1 lsl #9546355
+andvs r2, r1, r1 lsr #2
+andls r2, r1, r1 lsr #2
+andvs r2, r1, r1 lsr #3
+andls r2, r1, r1 lsr #3
+andvs r2, r1, r1 lsr #5
+andls r2, r1, r1 lsr #5
+andvs r2, r1, r1 lsr #9546355
+andls r2, r1, r1 lsr #9546355
+andvs r2, r1, r1 asr #2
+andls r2, r1, r1 asr #2
+andvs r2, r1, r1 asr #3
+andls r2, r1, r1 asr #3
+andvs r2, r1, r1 asr #5
+andls r2, r1, r1 asr #5
+andvs r2, r1, r1 asr #9546355
+andls r2, r1, r1 asr #9546355
+andvs r3, pc #9546355
+andls r3, pc #9546355
+andvss r1, r1 #1
+andlss r1, r1 #1
+andvss r1, r1 #5
+andlss r1, r1 #5
+andvss r1, r1 #9
+andlss r1, r1 #9
+andvss r1, r1 #15
+andlss r1, r1 #15
+andvss r1, r1 #2596896
+andlss r1, r1 #2596896
+andvss r2, r1, r1 lsl #2
+andlss r2, r1, r1 lsl #2
+andvss r2, r1, r1 lsl #3
+andlss r2, r1, r1 lsl #3
+andvss r2, r1, r1 lsl #5
+andlss r2, r1, r1 lsl #5
+andvss r2, r1, r1 lsl #9546355
+andlss r2, r1, r1 lsl #9546355
+andvss r2, r1, r1 lsr #2
+andlss r2, r1, r1 lsr #2
+andvss r2, r1, r1 lsr #3
+andlss r2, r1, r1 lsr #3
+andvss r2, r1, r1 lsr #5
+andlss r2, r1, r1 lsr #5
+andvss r2, r1, r1 lsr #9546355
+andlss r2, r1, r1 lsr #9546355
+andvss r2, r1, r1 asr #2
+andlss r2, r1, r1 asr #2
+andvss r2, r1, r1 asr #3
+andlss r2, r1, r1 asr #3
+andvss r2, r1, r1 asr #5
+andlss r2, r1, r1 asr #5
+andvss r2, r1, r1 asr #9546355
+andlss r2, r1, r1 asr #9546355
+andvss r3, pc #1
+andlss r3, pc #1
+andvcs r2, r1, r1 lsr #9546355
+andhis r2, r1, r1 lsr #9546355
+andges r2, r1, r1 lsr #9546355
+andlts r2, r1, r1 lsr #9546355
+andles r2, r1, r1 lsr #9546355
+andmis r2, r1, r1 lsr #9546355
+andpls r2, r1, r1 lsr #9546355
+andlos r2, r1, r1 lsr #9546355
+andvcs r2, r1, r1 lsr #9546355
+andhis r2, r1, r1 lsr #9546355
+andges r2, r1, r1 lsr #9546355
+andlts r2, r1, r1 lsr #9546355
+andles r2, r1, r1 lsr #9546355
+andmis r2, r1, r1 lsr #9546355
+andpls r2, r1, r1 lsr #9546355
+andlos r2, r1, r1 lsr #9546355
+andvcs r2, r1, r1 lsr #9546355
+andhis r2, r1, r1 lsr #9546355
+andges r2, r1, r1 lsr #9546355
+andlts r2, r1, r1 lsr #9546355
