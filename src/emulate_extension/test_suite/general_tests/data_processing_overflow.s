3439b172cf5190d18514b6e637d07503663fa8de
qs4617 <qianyi.shu17@imperial.ac.uk>
2018-06-05 11:57:32 +0100

    add test cases for basic calculation, overflow, borrow, effect of setting s bit, effect of comparison operation flag setting

--- src/emulate_extension/test_suite/general_tests/data_processing_overflow.s
+++ src/emulate_extension/test_suite/general_tests/data_processing_overflow.s
@@ -13,8 +13,8 @@ mov r11, #11
 mov r12, #12
 add r4, r2, r5 // no overflow
 add r6, r2, r6 // no overflow
-addc r5, r3, r7 // no overflow
-addc r11, r6, r3 // no overflow
+adc r5, r3, r7 // no overflow
+adc r11, r6, r3 // no overflow
 mov r0, #0             // basic test for arithmatic operation
 mov r1, #0xffffffff
 mov r2, #2
@@ -30,8 +30,8 @@ mov r11, #11
 mov r12, #12
 sub r12, r12, r3 // no borrow
 sub r9, r5, r4 // no borrow
-subc r7, r7, r3 // no borrow
-subc r6, r8, r5 // no borrow
+sbc r7, r7, r3 // no borrow
+sbc r6, r8, r5 // no borrow
 mov r0, #0                 // basic test for logical operation
 mov r1, #0xffffffff
 mov r2, #2
@@ -48,7 +48,6 @@ mov r12, #12
 and r2, r2, r2
 orr r7, r1, r7
 eor r3, r3, r4
-nor r6, r7, r6
 bic r3, r6, r9
 mov r0, #0
 mov r1, #0xffffffff
@@ -65,8 +64,8 @@ mov r11, #11
 mov r12, #12
 add r2, r1, r2 //overflow
 add r3, r1, r4 //overflow
-addc r4, r1, r5 //overflow
-addc r5, r1, r6 //overflow
+adc r4, r1, r5 //overflow
+adc r5, r1, r6 //overflow
 mov r0, #0
 mov r1, #0xffffffff
 mov r2, #2
@@ -83,7 +82,7 @@ mov r12, #12
 sub r2, r0, r1 //borrow
 sub r3, r4, r5 //borrow
 sub r4, r7, r12 // borrow
-subc r4, r3, r11 //borrow
+sbc r4, r3, r11 //borrow
 mov r0, #0
 mov r1, #0xffffffff
 mov r2, #2
@@ -99,7 +98,7 @@ mov r11, #11
 mov r12, #12
 rsb r0, r2, r1 // no borrow
 rsb r0, r2, r3 // no borrow
-rsbc r3, r9, r12 // no borrow
+rsc r3, r9, r12 // no borrow
 mov r0, #0
 mov r1, #0xffffffff
 mov r2, #2
@@ -115,7 +114,7 @@ mov r11, #11
 mov r12, #12
 rsb r0, r1, r2 // borrow
 rsb r0, r3, r2 // borrow
-rsbc r3, r12, r9 // borrow
+rsc r3, r12, r9 // borrow
 mov r0, #0
 mov r1, #0xffffffff
 mov r2, #2
@@ -130,9 +129,9 @@ mov r10, #10
 mov r11, #11
 mov r12, #12
 mul r0, r2, r1 // overflow
-mul r4, r4, r6 // no overflow
-mula r0, r2, r3, r1 // overflow
-mula r0, r10, r2, r9 // no overflow
+mul r4, r5, r6 // no overflow
+mla r0, r2, r3, r1 // overflow
+mla r0, r10, r2, r9 // no overflow
 mov r0, #0
 mov r1, #0xffffffff
 mov r2, #2
@@ -150,7 +149,7 @@ subs r2, r2, r2 // should set zero flag
 addeq r5, r6, r7 //should add
 addeqs r0, r2, r2 // should set zero flag
 subs r10, r12, r11
-addgt, r5, r6, r7 // should execute
+addgt r5, r6, r7 // should execute
 mov r0, #0
 mov r1, #0xffffffff
 mov r2, #2
@@ -198,3 +197,4 @@ mov r5, r2
 mov r5, r2
 mov r5, r2
 mov r5, r2
+mov r5, r3
