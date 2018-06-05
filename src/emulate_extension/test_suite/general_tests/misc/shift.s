edd86432709a4789e21217a4ed87ca3bf580ebfb
qs4617 <qianyi.shu17@imperial.ac.uk>
2018-06-05 13:06:47 +0100

    add test case specifically for shift

---
+++ src/emulate_extension/test_suite/general_tests/misc/shift.s
@@ -0,0 +1,201 @@
+mov r0, #0 // test shift specifically, if this work, then add and shift calculation should work
+mov r1, #0xffffffff
+mov r2, #2
+mov r3, #3
+mov r4, #4
+mov r5, #5
+mov r6, #6
+mov r7, #7
+mov r8, #8
+mov r9, #9
+mov r10, #10
+mov r11, #11
+mov r12, #12
+add r4, r2, lsl #12
+add r4, r2, lsl r8
+add r5, r2, lsr #12
+add r5, r2, lsr r8
+add r6, r2, asr #12
+add r6, r2, asr r8
+add r7, r2, ror #12
+add r4, r2, ror r8
+add r3, r2, asl #12
+add r3, r2, asl r8
+add r4, r2, lsl #0
+add r4, r2, lsl r0
+add r5, r2, lsr #0
+add r5, r2, lsr r0
+add r6, r2, asr #0
+add r6, r2, asr r0
+add r7, r2, ror #0
+add r4, r2, ror r0
+add r3, r2, asl #0
+add r3, r2, asl r8
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+mov r0, #0
+
