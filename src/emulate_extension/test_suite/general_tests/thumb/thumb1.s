04f57aa1c00c1a41281b2199ebac6b0dade681a7
Francis Nixon <fpn17@ic.ac.uk>
2018-06-06 07:38:33 +0000

    added some test files

--- src/emulate_extension/test_suite/general_tests/thumb/thumb1.s
+++ src/emulate_extension/test_suite/general_tests/thumb/thumb1.s
@@ -1,201 +1,196 @@
-mov r1, #34
-mov r2, #5
-mov r3, #23
-mov r4, #42
-mov r5, #64
-mov r6, #37
-mov r7, #7
-mov r8, #67
-mov r7, #54
-mov r8, #78
-mov r9, #54
-mov r10, #98
-mov r11, #2
-lsl r4, r4, #24
-asr r4, r1, #7
-asr r1, r6, #25
-asr r4, r1, #16
-lsl r7, r0, #5
-asr r2, r0, #5
-lsr r0, r5, #2
-lsr r5, r6, #19
-asr r6, r2, #25
-lsr r0, r5, #25
-lsl r6, r0, #3
-lsr r4, r6, #22
-lsl r3, r2, #29
-lsl r7, r2, #12
-lsr r7, r1, #21
-lsl r2, r5, #25
-asr r0, r3, #23
-asr r0, r6, #20
-lsr r6, r3, #11
-asr r4, r0, #5
-add r3, r4, r3
-sub r4, r2, r3
-add r1, r1, r3
-add r2, r6, #0
-add r0, r0, #12
-add r5, r0, #26
-add r0, r4, r1
-sub r7, r6, r0
-sub r0, r3, #11
-sub r6, r7, r0
-sub r0, r1, r7
-sub r7, r7, #5
-add r3, r4, r3
-sub r6, r2, #23
-sub r2, r4, #11
-sub r0, r4, r1
-sub r6, r7, #15
-sub r3, r5, r7
-add r2, r7, #18
-lsl r5, r5, #11
-lsl r4, r7, #8
-lsl r5, r1, #22
-asr r3, r6, #24
-asr r2, r7, #1
-lsr r1, r2, #3
-asr r4, r1, #1
-lsl r2, r3, #18
-lsl r7, r5, #30
-lsl r6, r0, #17
-lsr r0, r5, #10
-lsr r2, r1, #13
-lsr r4, r7, #20
-asr r5, r2, #20
-lsl r5, r6, #16
-lsl r7, r2, #14
-lsl r6, r3, #11
-asr r6, r5, #23
-lsl r3, r7, #22
-ror r4, r2
-lsl r1, r5
-mul r7, r2
-bic r4, r3
-lsl r4, r4
-tst r5, r0
-eor r7, r1
-bic r1, r1
-mul r4, r1
-neg r0, r1
-lsl r4, r7
-cmn r5, r3
-tst r5, r0
-adc r3, r7
-mul r4, r3
-bic r3, r2
+asr r2, r6, #17
+lsr r5, r4, #15
+lsl r4, r2, #18
+lsl r0, r0, #8
+asr r2, r2, #11
+lsr r4, r7, #4
+lsl r5, r1, #8
+lsr r3, r2, #22
+lsr r4, r7, #19
+asr r2, r3, #22
+asr r4, r5, #13
+lsl r5, r4, #28
+asr r4, r7, #27
+asr r2, r7, #17
+lsl r7, r4, #12
+asr r1, r6, #6
+asr r6, r0, #14
+asr r2, r3, #5
+asr r7, r4, #26
+asr r5, r1, #24
+sub r1, r1, #16
+add r7, r4, #30
+sub r2, r0, #21
+add r2, r1, #23
+sub r0, r5, r1
+add r0, r3, #3
+sub r5, r2, r4
+sub r3, r2, #22
+add r7, r1, r0
+sub r3, r2, r0
+add r1, r1, #12
+sub r2, r4, r6
+add r1, r7, r7
+sub r1, r6, r6
+sub r4, r4, #5
+sub r2, r2, #26
+sub r7, r1, r2
+sub r0, r5, r3
+sub r5, r7, r1
+asr r4, r2, #28
+lsr r3, r5, #13
+asr r6, r7, #12
+asr r5, r5, #25
+lsl r2, r2, #18
+lsl r1, r6, #27
+lsr r0, r4, #14
+lsr r2, r3, #30
+lsr r7, r0, #30
+lsl r7, r6, #14
+lsl r6, r7, #8
+lsl r5, r2, #19
+lsl r4, r4, #19
+lsr r0, r0, #3
+asr r1, r0, #6
+lsl r5, r1, #9
+lsl r1, r2, #26
+lsl r6, r6, #10
+lsl r3, r0, #5
+cmn r3, r5
 adc r7, r4
-and r5, r1
-neg r1, r6
-cmn r4, r5
-cmp r7, r7
-orr r2, r5
-orr r0, r6
-ror r0, r6
-mvn r3, r5
-asr r1, r5
-sbc r4, r6
-adc r4, r3
-lsl r7, r0
-cmp r1, r4
-mul r1, r2
-tst r7, r7
-cmn r5, r1
-lsr r3, r3
-asr r5, r3
-neg r0, r6
-ror r2, r6
-tst r1, r1
-orr r5, r3
-tst r9, r7
-orr r12, r3
-neg r2, r4
-ror r4, r4
-bic r14, r1
-lsl r0, r6
-eor r3, r5
-asr r3, r4
-neg r1, r3
-ror r4, r6
-cmp r8, r3
-tst r5, r13
-and r5, r2
-sbc r0, r2
-eor r11, r4
-eor r6, r4
-ror r10, r5
-and r5, r5
-ror r10, r10
-and r14, r13
-eor r10, r3
-eor r7, r11
-bic r10, r2
-sbc r14, r2
-mvn r11, r3
-adc r14, r1
-and r2, r5
-tst r11, r2
-mvn r1, r3
-mul r2, r0
-cmn r7, r1
-sbc r1, r12
-bic r10, r1
-orr r5, r11
-lsl r7, r2
-eor r1, r14
-eor r14, r5
-bic r1, r10
-tst r14, r10
-sbc r4, r14
-cmn r7, r4
-adc r3, r1
-mul r13, r2
-eor r5, r6
-lsr r13, r5
-mvn r1, r14
-bic r13, r3
-mul r5, r8
-lsr r0, r13
-orr r3, r4
-bic r11, r7
-cmn r0, r0
-ror r0, r2
-mvn r5, r7
-sbc r12, r8
-lsr r10, r9
-lsl r14, r13
-adc r6, r6
-sbc r11, r2
+and r7, r2
+eor r5, r3
+tst r5, r2
+sbc r2, r5
 orr r5, r5
-orr r2, r7
-ror r5, r8
-asr r14, r4
-tst r4, r7
-eor r5, r2
-neg r12, r1
-adc r1, r8
+mul r1, r1
+ror r1, r0
+adc r0, r5
+tst r0, r5
+lsl r1, r4
+eor r7, r7
+sbc r6, r5
+asr r3, r4
+bic r2, r5
+tst r7, r0
+mul r3, r2
+bic r0, r3
+cmn r4, r0
+mul r6, r4
+bic r7, r4
+ror r5, r3
+tst r2, r1
+neg r3, r6
+lsr r7, r6
+mvn r2, r5
+cmn r7, r7
+lsl r1, r1
+and r3, r0
+cmp r0, r6
+mvn r0, r2
+mul r0, r0
+ror r0, r0
+orr r1, r6
+eor r7, r3
+lsl r7, r5
+eor r2, r4
+sbc r6, r4
+tst r0, r0
+cmn r0, r4
+orr r5, r6
+and r3, r7
+lsr r5, r5
+mul r3, r3
+adc r6, r4
+bic r3, r2
+asr r4, r4
+mul r5, r1
+lsl r6, r6
+cmn r6, r2
+orr r1, r7
+asr r6, r6
+lsr r5, r2
+orr r7, r2
+lsr r3, r3
+bic r1, r4
+cmn r1, r0
+lsl r2, r4
+ror r2, r7
+adc r4, r5
+ror r5, r7
+mvn r5, r6
+tst r0, r3
+adc r1, r0
+eor r0, r5
+mvn r4, r3
+and r4, r5
+lsl r3, r3
+mvn r3, r3
+cmp r6, r6
+asr r0, r0
+tst r1, r0
+mul r3, r7
+mul r7, r7
+bic r7, r7
+lsr r1, r1
+mul r2, r3
+mvn r4, r4
+ror r4, r0
+mvn r5, r1
+and r7, r2
+and r6, r2
+tst r5, r2
+tst r3, r3
+asr r7, r2
+cmn r5, r7
+adc r3, r0
+cmp r2, r6
+bic r6, r5
+mvn r2, r3
+tst r2, r4
+cmp r6, r6
+orr r1, r5
+lsl r6, r3
+mvn r4, r7
+ror r7, r2
+orr r4, r6
+mvn r2, r5
+ror r5, r0
+cmn r1, r2
+adc r4, r4
+tst r7, r2
+mvn r4, r6
+tst r4, r4
+cmn r2, r2
+mul r4, r3
+lsr r7, r5
+bic r4, r6
+adc r2, r3
+mul r5, r2
+mul r5, r5
+sbc r5, r1
+eor r7, r3
+adc r7, r3
+asr r5, r4
+adc r3, r3
+mul r5, r5
+cmp r2, r3
+cmn r1, r4
+mvn r0, r3
+cmn r1, r6
+mvn r3, r5
+cmp r7, r0
+cmn r5, r4
+orr r7, r3
+eor r7, r4
+orr r3, r0
+lsl r1, r4
 orr r5, r1
-and r4, r3
-adc r0, r3
-lsl r13, r6
-orr r2, r11
-mvn r8, r1
-mul r13, r1
-tst r6, r1
-mul r8, r1
-adc r13, r7
-cmn r11, r1
-cmn r10, r4
-mul r13, r2
-neg r12, r0
-and r9, r4
-lsr r1, r5
-mvn r14, r8
-cmp r9, r6
-cmp r9, r4
-cmp r9, r7
-cmp r9, r8
-cmp r9, r0
-cmp r9, r1
-
+neg r1, r3
+asr r4, r3
+and r3, r3
+cmn r0, r2
+and r3, r6
+mul r1, r0
+mul r0, r1
+lsl r5, r3
