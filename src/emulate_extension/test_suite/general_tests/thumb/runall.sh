b14b314af00273838a0a4463c389b0a8fe2f7599
Francis Nixon <fpn17@imperial.ac.uk>
2018-06-09 16:40:23 +0100

    fixed bug in thumb runall script.

--- src/emulate_extension/test_suite/general_tests/thumb/runall.sh
+++ src/emulate_extension/test_suite/general_tests/thumb/runall.sh
@@ -1,1 +1,1 @@
-for i in *.bin2; do echo $i;python3 ../comparison_script.py /homes/fpn17/CLionProjects/arm11_47/src/cmake-build-debug/emulate_extension $(basename $i .bin2);  done
+for i in *.bin2; do echo $i;python3 comparison_script.py /homes/fpn17/CLionProjects/arm11_47/src/cmake-build-debug/emulate_extension $(basename $i .bin2);  done
