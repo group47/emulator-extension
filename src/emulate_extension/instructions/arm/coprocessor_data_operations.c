4a3b10789f4a89c2c0ac2d513f483e73bba002ae
Francis Nixon <fpn17@imperial.ac.uk>
2018-06-09 16:54:43 +0100

    added more verbose logging to coprocessor instructions, which will be useful for debugging boot.

--- src/emulate_extension/instructions/arm/coprocessor_data_operations.c
+++ src/emulate_extension/instructions/arm/coprocessor_data_operations.c
@@ -16,6 +16,12 @@ execute_instruction_coprocessor_data_operations(struct CoprocessorDataOperations
 
     fprintf(get_logfile(), "Try to execute coprocessor data operations\n");
     fprintf(get_logfile(), "Coprocessor number : %d\n", instruction.CoNumber);
+    fprintf(get_logfile(), "Coprocessor destination register: %d\n", instruction.CoDestinationRegister);
+    fprintf(get_logfile(), "Coprocessor cooperand register position 3: %d\n", instruction.CoOperandRegister_position3);
+    fprintf(get_logfile(), "Coprocessor cooperand register position 19: %d\n",
+            instruction.CoOperandRegister_position19);
+    fprintf(get_logfile(), "Coprocessor operation code: %d\n", instruction.CoOperationCode);
+    fprintf(get_logfile(), "Coprocessor information: %d\n", instruction.CoInformation);
     assert(false);
 
     // todo: fed the instruction to the coprocessor
--- src/emulate_extension/instructions/arm/coprocessor_data_transfers.c
+++ src/emulate_extension/instructions/arm/coprocessor_data_transfers.c
@@ -2,8 +2,11 @@
 // Created by qs4617 on 02/06/18.
 //
 
+#include <assert.h>
+#include <stdio.h>
 #include "coprocessor_data_transfers.h"
 #include "../../util/cpsr_util.h"
+#include "../../util/entry_point.h"
 
 
 enum ExecutionExitCode
@@ -12,5 +15,17 @@ execute_instruction_coprocessor_data_transfers(struct CoprocessorDataTransfersIn
         return DIDNT_EXECUTE;
     }
 
+    fprintf(get_logfile(), "Coprocesor number %d\n", instruction.CoNumber);
+    fprintf(get_logfile(), "Coprocesor base register %d\n", instruction.baseRegister);
+    fprintf(get_logfile(), "Coprocesor source destination register %d\n", instruction.CoSourceDestinationRegister);
+    fprintf(get_logfile(), "Coprocesor load store bit %d\n", instruction.loadStoreBit);
+    fprintf(get_logfile(), "Coprocesor offset %d\n", instruction.offset);
+    fprintf(get_logfile(), "Coprocesor pre post indexing %d\n", instruction.prePostIndexingBit);
+    fprintf(get_logfile(), "Coprocesor transfer length %d\n", instruction.transferLength);
+    fprintf(get_logfile(), "Coprocesor up down bit %d\n", instruction.upDownBit);
+    fprintf(get_logfile(), "Coprocesor write back bit %d\n", instruction.writeBackBit);
+
+
+    assert(false);
     return OK;
 }
